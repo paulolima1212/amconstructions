df88aa817a349824fbeafdb6978b7b79106ea231
paulolima1212 <plima12121984@gmail.com>
2023-02-04 13:46:46 +0000

    test: create new page product

---
+++ src/pages/createproduct/index.page.tsx
@@ -0,0 +1,65 @@
+import { api } from '@/lib/axios.config'
+import { ArrowLeft } from 'phosphor-react'
+import { Button, Form, FormContainer, Input, Select, Title } from './styles'
+import Link from 'next/link'
+
+export default function FormNewProduct() {
+  async function handleCreateNewProduct(data: any) {
+    // const { name, provider, price, measure, family } = data
+    // api
+    //   .post(`/api/createProduct`, {
+    //     name,
+    //     provider,
+    //     price,
+    //     measure,
+    //     family,
+    //   })
+    //   .then((response) => {
+    //     return response.data
+    //   })
+  }
+
+  return (
+    <FormContainer>
+      <Title>
+        <Link href={'/'}>
+          <ArrowLeft size={32} />
+        </Link>
+        Nova Entrada
+      </Title>
+      <Form onSubmit={handleCreateNewProduct}>
+        <label htmlFor="product">
+          <span>Produto</span>
+          <Input type="text" placeholder="Digite o nome do produto" />
+        </label>
+        <label htmlFor="price">
+          <span>Preço</span>
+          <Input
+            type="number"
+            placeholder="Digite o preço do produto"
+            min={0.01}
+            step={0.01}
+          />
+        </label>
+        <label htmlFor="provider">
+          <span>Fornecedor</span>
+          <Input type="text" placeholder="Digite o nome do fornecedor" />
+        </label>
+        <label htmlFor="family">
+          <span>Categoria</span>
+          <Input type="text" placeholder="Digite o nome do fornecedor" />
+        </label>
+        <label>
+          <span>Un.</span>
+          <Select aria-label="measure-unit">
+            <option value="UN">UN</option>
+            <option value="KG">KG</option>
+            <option value="M2">M²</option>
+            <option value="M">M</option>
+          </Select>
+        </label>
+        <Button type="submit">Cadastrar</Button>
+      </Form>
+    </FormContainer>
+  )
+}
