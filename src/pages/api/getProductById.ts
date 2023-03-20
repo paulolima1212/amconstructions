4cea60c82e0c459d0a2593e0122a05106f09d70c
paulolima1212 <plima12121984@gmail.com>
2023-03-20 14:06:04 +0000

    bug-fix: fix lint

--- src/pages/api/getProductById.ts
+++ src/pages/api/getProductById.ts
@@ -1,5 +1,4 @@
 import { prisma } from '@/lib/prisma'
-import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handleGetProductById(id: string) {
   const product = await prisma.product.findUnique({
